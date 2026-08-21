a812259f71022ae4c7453268ee71814829c962d3
sn-vuoch <srinnaravuoch@gmail.com>
2026-08-21 16:57:24 +0700

    Create the operate function

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ function divide(num1, num2) {
   }
   return num1 / num2;
 }
+
+function operate(operator, num1, num2) {
+  let result;
+  switch (operator) {
+    case "+":
+      result = add(num1, num2);
+      break;
+    case "-":
+      result = subtract(num1, num2);
+      break;
+    case "*":
+      result = multiply(num1, num2);
+      break;
+    case "/":
+      result = divide(num1, num2);
+      break;
+  }
+  return result;
+}
